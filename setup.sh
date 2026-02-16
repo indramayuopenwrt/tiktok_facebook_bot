8cb0f2c18fa7400fbc84e04545ec92afa765039e
indramayuopenwrt <cloudflareworkers173@gmail.com>
2026-02-16 11:05:27 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
-# Install ffmpeg
+# Update paket
 apt-get update
+
+# Install ffmpeg
 apt-get install -y ffmpeg
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Install moviepy
+pip install moviepy
